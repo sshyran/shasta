95dad1e4222df9cc329ebcf9449a357fa52fbb2a
Paolo Carnevali <pacarnev@ucsc.edu>
2022-11-17 07:50:59 -0800

    Add bitReversal.hpp, without implementation.

---
+++ src/bitReversal.hpp
@@ -0,0 +1,13 @@
+#ifndef SHASTA_BIT_REVERSAL_HPP
+#define SHASTA_BIT_REVERSAL_HPP
+
+namespace shasta {
+    inline uint8_t bitReversal(uint8_t);
+    inline uint16_t bitReversal(uint16_t);
+    inline uint32_t bitReversal(uint32_t);
+    inline uint32_t bitReversal(uint64_t);
+}
+
+
+
+#endif
